79e2e08173186254dddb37a9470026873cc5ea0f
hlk <1747180246@qq.com>
2019-07-01 11:12:06 +0800

    输出

---
+++ src/com/oaec/mybatis/entity/Book.java
@@ -0,0 +1,50 @@
+package com.oaec.mybatis.entity;
+
+public class Book {
+    private int bookId;
+    private String name;
+    private String author;
+    private double price;
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/com/oaec/mybatis/entity/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.oaec.mybatis.entity;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class HelloWorld {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Object o = sqlSession.selectOne("hello.world", 1);
+        System.out.println("o = " + o);
+        sqlSession.close();
+        inputStream.close();
+    }
+}
